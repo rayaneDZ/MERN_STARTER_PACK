0039311fea88e4b6548b55dd6344b3861e7a3fcb
Rayane Bouthiba <chlef-algerie@hotmail.fr>
2018-08-19 14:34:03 +0100

    done

--- client/src/components/customers.js
+++ client/src/components/customers.js
@@ -1,29 +1,55 @@
 import React, { Component } from 'react';
 import './customers.css';
+import axios from 'axios';
 
 class Customers extends Component {
   constructor() {
     super();
     this.state = {
-      customers: []
+      customers: null,
+      inputVal : ''
     };
+    this.addCustomer = this.addCustomer.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  componentDidMount() {
+  componentWillMount() {
     fetch('/api/customers')
-      .then(res => res.json())
-      .then(customers => this.setState({customers}, () => console.log('Customers fetched...', customers)));
+      .then(res => {
+        return res.json();
+      })
+      .then(customers => this.setState({customers}));
   }
 
+  addCustomer(){
+    axios.post('/api/customers', {
+      "firstname": "houda",
+      "lastname": "slamani"
+    })
+    .then(result => {
+      console.log(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+  handleChange(e){
+    let value = e.target.value;
+    this.setState({
+      inputVal : value
+    });
+  }
   render() {
     return (
       <div>
         <h2>Customers</h2>
-        <ul>
+        <button type='button' onClick={this.addCustomer}>add customer</button>
+        <input type='input' value={this.state.inputVal} onChange={this.handleChange}/>
+        {this.state.customers && <ul>
         {this.state.customers.map(customer => 
-          <li key={customer.id}>{customer.firstName} {customer.lastName}</li>
+          <li key={customer._id}>{customer.firstname} {customer.lastname}</li>
         )}
-        </ul>
+        </ul>}
       </div>
     );
   }
--- server.js
+++ server.js
@@ -1,15 +1,128 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
+
+mongoose.connect('mongodb+srv://rayane_admin:QpS10qjZxpHbdlK4@rayanesdatabase-edxog.mongodb.net/test?retryWrites=true', {
+  useNewUrlParser : true
+}); 
+
+app.use(bodyParser.json());
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//   );
+//   if (req.method === 'OPTIONS') {
+//     res.header(
+//       'Access-Control-Allow-Methods',
+//       'PUT, POST, PATCH, DELETE, GET'
+//     );
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
+
+// app.use(cors());
+// app.options('*',cors());
+const User = require('./models/Users');
+
+app.get('/api/test', (req, res) => {
+console.log(mongoose.connection.readyState);
+res.send('hi');
+});
 
 app.get('/api/customers', (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
+  User.find()
+    .exec()
+    .then( result => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error : err
+      });
+    });
+});
 
-  res.json(customers);
+app.get('/api/customers/:id', (req, res) => {
+  console.log('getting by id');
+  const id =req.params.id;
+  User.findById(id)
+    .exec()
+    .then(result => {
+      console.log(result);
+      if (result) {
+        res.status(200).json(result);
+      }else{
+        res.status(404).json({message : 'no valid entry found'});
+      }
+    })
+    .catch(err => {
+      console.log('error');
+      console.log(err);
+      res.status(500).json({ error : err});
+    });
+});
+
+
+app.post('/api/customers', (req, res) => {
+
+  const user = new User({
+    _id : new mongoose.Types.ObjectId(),
+    firstname : req.body.firstname,
+    lastname : req.body.lastname
+  });
+  user
+    .save()
+    .then(result => {
+      console.log(result, 'done post');
+      res.status(201).json({
+        message : 'handling post req',
+        createdUser : user
+      });
+    })
+    .catch(err => console.log(err));
+});
+
+app.delete('/api/customers/:id', (req, res) => {
+  const id = req.params.id;
+  User.remove({_id : id})
+    .exec()
+    .then( result => {
+      res.status(200).json(result);
+    })
+    .catch( err => {
+      console.log(err);
+      res.status(500).json({
+        error : err
+      });
+    });
+});
+
+app.patch('/api/customers/:id', (req, res) => {
+  const id = req.params.id;
+  const updateOps = {};
+  for(const ops of req.body){
+    updateOps[ops.propName] = ops.value
+  }
+  User.update({_id: id}, {$set: updateOps})
+    .exec()
+    .then(result => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error : err
+      });
+    });
 });
 
 const port = 5000;
